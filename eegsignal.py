bc4f3f6b125cb9b375ba7d35cf3470b1f1b40019
Tyson Burghardt <tyson DOT burghardt AT gmail.com>
2016-12-16 11:13:19 -0500

    renamed

---
+++ eegsignal.py
@@ -0,0 +1,23 @@
+import scipy.signal as sn
+
+
+class Signal(object):
+    def __init__(self, samplingRate, data):
+        self.sampR = samplingRate
+        self.data = data
+        self.hpass = self.nfreq(70.0)
+        self.lpass = self.nfreq(1.0)
+        self.fcoeffs = sn.iirfilter(2, [self.lpass, self.hpass])
+        self.cache = None
+            
+    def nfreq(self, freq):
+        """returns the frequency normalized to 1 = Nyquist"""
+        return freq/(0.5*self.sampR)
+            
+    def getRawData(self):
+        return self.data
+    
+    def getFiltData(self):
+        self.cache = sn.filtfilt(self.fcoeffs[0], self.fcoeffs[1], self.getRawData()) if self.cache is None
+        return self.cache
+            
