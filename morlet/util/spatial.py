42ffc79e609379aecd2c3cd673c0e681d5b6a956
Tyson Burghardt <tyson DOT burghardt AT gmail.com>
2017-03-30 13:53:15 -0400

    started work on spatial definitions of electrodes

---
+++ morlet/util/spatial.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def tenten(u,v):
+    """u are the transverse lines from -2 (Fpx) to +2 (Ox) and v are the
+    longitudes left and right of the midline (-3 to +3 for 9/10, 7, 3, and
+    z)."""
+    while (-2 <= u <= 2 and -3 <= v <= 3):
+        
+        pass
+
+Cz = 0,0,1
+Pz = pi, pi/5, 1
+Oz = pi, 2*pi/5, 1
+Fz = 0, pi/5, 1
+Fpz = 0, 2*pi/5, 1
+T9 = -pi/2, pi/2, 1
+T10 = pi/2, pi/2, 1
+
