5599b78205708d9479e2a5926a978147d973df10
Tyson Burghardt <tyson.burghardt@gmail.com>
2017-04-01 10:03:28 -0400

    just use scipy's stft

--- morlet/processing/stft.py
+++ morlet/processing/stft.py
@@ -7,26 +7,12 @@ times
 
 #short-term fourier transform
 
-import numpy as np
-import scipy
+import scipy.signal as sgn
 import utilities
-
-def stft(n, step, overlap):
-    """calculates short-time fourier transform for an array (assumed real)
-    given the @step in samples, and the @overlap in samples."""
-    eStep = step - overlap
-    times = len(n) / (step-(overlap/2))
-    winfun = scipy.hamming(step)
-    coeffs = utilities.sigprocHalf(step)
-    output = np.zeros((times+1, 2, coeffs))
-    for i in xrange(times+1):
-        o = scipy.fft(winfun*n[i*eStep:i*eStep+step], n=step)
-        output[i] = np.vstack( (np.abs(o[:coeffs]), np.angle(o[:coeffs])) )
-    return output
 
 def std_stft(data, fs):
     """calculates STFT for a real array with default step size fs/2 and overlap
     fs/4."""
-    return stft(data, fs/2., fs/4.)
+    return sgn.stft(data, nperseg=fs//2)
 
 
